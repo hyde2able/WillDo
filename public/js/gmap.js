7fc05b1ebbe8f65008f77d2c1cd77ff8a024b277
hyde2able <hyde31415@gmail.com>
2015-11-11 02:39:43 +0900

    クリックした場所へのルート検索を表示完了
    GoogleMapのAPIを用いて、クリックした場所へのルート検索実装。

--- public/js/gmap.js
+++ public/js/gmap.js
@@ -2,12 +2,12 @@ $(function() {
 
 	/* 検索した現在位置 */
 	var StartLatLng = new google.maps.LatLng( parseFloat( $('#IamHere').attr('lat') ), parseFloat( $('#IamHere').attr('lng') ) );
-
-	var map
-	,	directionsRenderer
-	,	directions
+	var directionsDisplay
+	,	directionsService = new google.maps.DirectionsService()
+	,	map
 	,	err = google.maps.DirectionsStatus
 	,	directionsErr = new Array();
+
 	directionsErr[err.INVALID_REQUEST] = "指定された DirectionsRequest が無効です。";
 	directionsErr[err.MAX_WAYPOINTS_EXCEEDED] = "DirectionsRequest に指定された DirectionsWaypoint が多すぎます。ウェイポイントの最大許容数は 8 に出発地点と到着地点を加えた数です。";
 	directionsErr[err.NOT_FOUND] = "出発地点、到着地点、ウェイポイントのうち、少なくとも 1 つがジオコード化できませんでした。";
@@ -16,37 +16,71 @@ $(function() {
 	directionsErr[err.UNKNOWN_ERROR] = "サーバー エラーのため、ルート リクエストを処理できませんでした。もう一度試すと正常に処理される可能性があります。";
 	directionsErr[err.ZERO_RESULTS] = "出発地点と到着地点間でルートを見つけられませんでした。";
 
-	initialize();
+	//initialize();
 
+	google.maps.event.addDomListener(window, 'load', initialize() );
 	/* 地図の初期化  */
 	function  initialize() {
 		var myOptions = {
-			zoom: 17,
+			zoom: 15,
 			center: StartLatLng,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			scaleControl: true
 		};
-		map = new google.maps.Map( document.getElementById("map"), myOptions);		// Google Maps作成
-
-		// ルートレンダラー生成
-		directionsRenderer = new google.maps.DirectionsRenderer( {
-			polylineOptions: {
-				strokeColor: "#FF0000",
-				strokeWeight: 4,
-				strokeOpacity: 0.7
-			}
-		});
-		directionsRenderer.setMap(map);			// ルートレンダラーをマップに関連付け		
+		map = new google.maps.Map( document.getElementById("gmap"), myOptions);		// Google Maps作成	
+		render();	
 	};
 
+	/* ルート検索結果を描画 */
+	function render() {
+		directionsDisplay = new google.maps.DirectionsRenderer({
+			"map": map,
+			"preserveViewport": true,
+			"draggable": true
+		});
+		directionsDisplay.suppressMarkers = true; 
+		var originMarker = CreateMarker( StartLatLng ); 
+		/* 右カラムにルート表示 */
+		// directionsDisplay.setPanel( document.getElementById('directions_panel') );
+
+
+	};
+    
+    /* マーカーを作成 */
+    function CreateMarker(LatLng) {
+    	var marker = new google.maps.Marker( {
+    		position: LatLng,
+    		map: map,
+    		draggable: true
+    	});
+    	// google.maps.event.addListener(marker, "dragend", UpdateRoute); 
+    	return marker;
+    };
+
+ 
+        // // 現在のマーカー位置でルートを更新します。 
+        // function UpdateRoute() 
+        // { 
+        //     var request = { 
+        //         origin: originMarker.getPosition(), 
+        //         destination: destinationMarker.getPosition(), 
+        //         travelMode: google.maps.DirectionsTravelMode.DRIVING 
+        //     }; 
+        //     directionsService.route(request, function(result, status) 
+        //     { 
+        //         if (status == google.maps.DirectionsStatus.OK) 
+        //         { 
+        //             directionsRenderer.setDirections(result); 
+        //             SetDistance(result); 
+        //         } 
+        //     }); 
+        // } 
 
 	/* ルート検索 */
 	function searchRoute(lat, lng) {
 		var EndLatLng = new google.maps.LatLng( parseFloat(lat), parseFloat(lng) );
 
-		// 検索設定 ルート生成
-		directions = new google.maps.DirectionsService();
-		// ルートリクエスト
-		directions.route( {
+		var request = {
 			origin: StartLatLng,		// 開始位置
 			destination: EndLatLng, 	// 終了位置　
 			travelMode: google.maps.DirectionsTravelMode.WALKING,	// ルートタイプ(徒歩)　車ならDRIVING
@@ -54,10 +88,13 @@ $(function() {
 			optimizeWaypoints: true,	// 最適化された最短距離にする
 			avoidHighways: true,		// 高速道路は使わない
 			avoidTolls: true			// 有料道路は使わない
-		},
-		function(results, status){
-			if( status == err.OK ) {	// 検索結果がtrueの場合
-				directionsRenderer.setDirections(results);
+		};
+		var distinationMarker = CreateMarker( EndLatLng ); 
+
+		// ルート描画
+		directionsService.route(request, function(response, status) {
+			if( status == google.maps.DirectionsStatus.OK ) {
+				directionsDisplay.setDirections(response);
 			} else {
 				var message = directionsErr[status];
 				$('#messages').append('<div class="alert alert-warning alert-dismisable" role="alert" id="alert"><button type="button" class="close" data-dismiss="alert" aria-label="閉じる"><span aria-hidden="true">×</span></button><p><strong>warning</strong>：' + message + '</p></div>');
@@ -65,11 +102,13 @@ $(function() {
 		});
 	};
 
+
 	/* willdoをクリックしたらそのlatとlngを取得してルート検索 */
 	$(document).on("click", "#willdo", (function() {
 		var lat = $(this).attr('lat')
 		,	lng = $(this).attr('lng');
 
+		console.log(lat + lng);
 		searchRoute(lat, lng);
 	}));
 
